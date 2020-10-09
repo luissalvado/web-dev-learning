843f632c2ebaca9aba62709da2ddcc3a02534fcb
luissalvado <luismiguelsalvado@gmail.com>
2020-10-09 12:31:22 +0100

    filtering content.

--- Angular/01-Learning_Angular/01-Using_Angular_CLI/learnangular5/src/app/artist-items/artist-items.component.ts
+++ Angular/01-Learning_Angular/01-Using_Angular_CLI/learnangular5/src/app/artist-items/artist-items.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-artist-items',
   templateUrl: './artist-items.component.html',
-  inputs: ['artist']
 })
 export class ArtistItemsComponent implements OnInit {
+
+  @Input() artist: any = '';
 
   constructor() { }
 
---
+++ Angular/01-Learning_Angular/01-Using_Angular_CLI/learnangular5/src/app/search-artists.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'searchArtists'
+})
+export class SearchArtistsPipe implements PipeTransform {
+
+  transform(pipeData: any, pipeModifier: any): any {
+    return pipeData.filter((eachItem: { [x: string]: string; }) => {
+      return (
+        eachItem['name'].toLowerCase().includes(pipeModifier.toLowerCase()) ||
+        eachItem['reknown'].toLowerCase().includes(pipeModifier.toLowerCase())
+      )
+    });
+  }
+}
