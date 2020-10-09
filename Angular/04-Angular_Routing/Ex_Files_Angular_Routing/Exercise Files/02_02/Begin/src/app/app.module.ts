2bb2ca099f23e192218fc5742a0717b74935ae04
luissalvado <luismiguelsalvado@gmail.com>
2020-10-09 18:10:23 +0100

    router setup and configuration.

--- Angular/04-Angular_Routing/Ex_Files_Angular_Routing/Exercise Files/02_02/Begin/src/app/app.module.ts
+++ Angular/04-Angular_Routing/Ex_Files_Angular_Routing/Exercise Files/02_02/Begin/src/app/app.module.ts
@@ -6,14 +6,27 @@ import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {ResetComponent} from "./reset-password/reset-password.component";
 import {OutletComponent} from "./outlet/outlet.component";
-import {NotFoundComponent} from "./404/404.component";
+import { NotFoundComponent } from './404/404.component';
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'reset-password', component: ResetComponent },
+  { path: '**', component:  NotFoundComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
+    OutletComponent,
+    LoginComponent,
+    ResetComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
