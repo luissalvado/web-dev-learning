d8a6374b1aea676d9d5e0b4603604d2bc9a66bfb
luissalvado <luismiguelsalvado@gmail.com>
2020-10-16 13:45:44 +0100

    added product-service.

--- Angular/99-Angular_9/crud/frontend/src/app/components/product/products-create/products-create.component.ts
+++ Angular/99-Angular_9/crud/frontend/src/app/components/product/products-create/products-create.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from "./../product.service";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-products-create',
-  templateUrl: './products-create.component.html',
-  styleUrls: ['./products-create.component.css']
+  selector: "app-products-create",
+  templateUrl: "./products-create.component.html",
+  styleUrls: ["./products-create.component.css"],
 })
 export class ProductsCreateComponent implements OnInit {
+  constructor(private productService: ProductService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  createProduct(): void {
+    this.productService.showMessage("Product Created !");
   }
 
+  cancelProduct(): void {
+    this.router.navigate(["/products"]);
+  }
 }
