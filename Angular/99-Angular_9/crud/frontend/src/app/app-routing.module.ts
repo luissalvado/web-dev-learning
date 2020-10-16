08c69982e771d5c4f3217fefb30d6e6fd04c944f
luissalvado <luismiguelsalvado@gmail.com>
2020-10-16 11:47:08 +0100

    create a new component product-create and made the navigation to him.

--- Angular/99-Angular_9/crud/frontend/src/app/app-routing.module.ts
+++ Angular/99-Angular_9/crud/frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
 import { HomeComponent } from "./views/home/home.component";
+import { ProductsCreateComponent } from './components/product/products-create/products-create.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
     path: "products",
     component: ProductCrudComponent,
   },
+  {
+    path: "products/create",
+    component: ProductsCreateComponent,
+  }
 ];
 
 @NgModule({
--- Angular/99-Angular_9/crud/frontend/src/app/views/product-crud/product-crud.component.ts
+++ Angular/99-Angular_9/crud/frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-product-crud',
-  templateUrl: './product-crud.component.html',
-  styleUrls: ['./product-crud.component.css']
+  selector: "app-product-crud",
+  templateUrl: "./product-crud.component.html",
+  styleUrls: ["./product-crud.component.css"],
 })
 export class ProductCrudComponent implements OnInit {
+  // inject router in constructor
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  //using the router
+  navigateToProductCreate(): void {
+    this.router.navigate(["/products/create"]);
   }
-
 }
