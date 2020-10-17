ad2d887324c4825e3321d381ed72790d65837dae
luissalvado <luismiguelsalvado@gmail.com>
2020-10-17 12:09:21 +0100

    added method delete to exclude products in the back-end.

--- Angular/99-Angular_9/crud/frontend/src/app/components/product/product.service.ts
+++ Angular/99-Angular_9/crud/frontend/src/app/components/product/product.service.ts
@@ -1,42 +1,47 @@
-import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar'
-import { HttpClient } from '@angular/common/http';
-import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { HttpClient } from "@angular/common/http";
+import { Product } from "./product.model";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root' // Singleton 
+  providedIn: "root", // Singleton
 })
 export class ProductService {
+  baseUrl = "http://localhost:3001/products";
 
-  baseUrl = 'http://localhost:3001/products'
-
-  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
   // show a pop message when product created.
-  showMessage(msg: string): void{
-    this.snackBar.open(msg, 'X', {
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, "X", {
       duration: 3000,
       horizontalPosition: "right",
-      verticalPosition: "top"
-    })
+      verticalPosition: "top",
+    });
   }
   // return a observable of Product.
   create(product: Product): Observable<Product> {
     return this.http.post<Product>(this.baseUrl, product);
   }
 
-  read(): Observable<Product[]>{
-    return this.http.get<Product[]>(this.baseUrl)
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
   }
 
-  readByID(id: string): Observable<Product>{
-    const url = `${this.baseUrl}/${id}`
-    return this.http.get<Product>(url)
+  readByID(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url);
   }
 
-  update(product: Product): Observable<Product>{
-    const url = `${this.baseUrl}/${product.id}`
-    return this.http.put<Product>(url, product)
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`;
+    return this.http.put<Product>(url, product);
+  }
+
+  //nao é necessario ser o produto todo, basta o id
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
   }
 }
