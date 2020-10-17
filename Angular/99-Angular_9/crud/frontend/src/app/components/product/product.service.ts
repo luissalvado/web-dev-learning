55fe87b2b45fe10ea5aa59b0978f33bbb6ed538b
luissalvado <luismiguelsalvado@gmail.com>
2020-10-17 11:33:44 +0100

    added readById() and update() in product.service

--- Angular/99-Angular_9/crud/frontend/src/app/components/product/product.service.ts
+++ Angular/99-Angular_9/crud/frontend/src/app/components/product/product.service.ts
@@ -30,4 +30,13 @@ export class ProductService {
     return this.http.get<Product[]>(this.baseUrl)
   }
 
+  readByID(id: string): Observable<Product>{
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+
+  update(product: Product): Observable<Product>{
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
 }
