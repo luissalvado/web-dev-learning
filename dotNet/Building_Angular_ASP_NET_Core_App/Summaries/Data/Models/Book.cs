c7cdd9fb8078bfc6da7a693f316abbc23367c6e9
luissalvado <luismiguelsalvado@gmail.com>
2020-10-21 14:53:49 +0100

    Creating data models.

---
+++ dotNet/Building_Angular_ASP_NET_Core_App/Summaries/Data/Models/Book.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Summaries.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        //These props are optional.
+        public double? Rate { get; set; }
+        public DateTime? DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
+    }
+}
