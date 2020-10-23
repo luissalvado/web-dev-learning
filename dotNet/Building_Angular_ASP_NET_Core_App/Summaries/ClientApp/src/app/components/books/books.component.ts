8c307fe9d73e36d4ef00d1ca0fcad09c17cff020
luissalvado <luismiguelsalvado@gmail.com>
2020-10-23 09:30:39 +0100

    Designing the Create page.

--- dotNet/Building_Angular_ASP_NET_Core_App/Summaries/ClientApp/src/app/components/books/books.component.ts
+++ dotNet/Building_Angular_ASP_NET_Core_App/Summaries/ClientApp/src/app/components/books/books.component.ts
@@ -1,15 +1,15 @@
-import { BookService } from "./../../services/book.service";
-import { Component, OnInit } from "@angular/core";
+import { BookService } from './../../services/book.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-books",
-  templateUrl: "./books.component.html",
-  styleUrls: ["./books.component.css"],
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
   public books: Book[];
 
-  constructor(private service: BookService) {}
+  constructor(private service: BookService) { }
 
   ngOnInit() {
     this.service.getAllBooks().subscribe((data) => {
